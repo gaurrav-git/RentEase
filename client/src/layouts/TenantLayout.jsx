f2c497d5e32bef7d5ab4c0d76fc694e9943345df
Gaurav Arora <gaurrav.git@gmail.com>
2026-07-26 20:14:57 +0530

    Refine dashboard stat card labels

--- client/src/layouts/TenantLayout.jsx
+++ client/src/layouts/TenantLayout.jsx
@@ -1,4 +1,4 @@
-import Sidebar from "../components/TenantSidebar";
+import Sidebar from "../components/tenantSidebar";
 import Navbar from "../components/Navbar";
 
 function TenantLayout({ children }) {
